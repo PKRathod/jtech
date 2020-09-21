a14b9bc8d88b03b78675270090e7938d936cb397
Praveen <Praveen@DESKTOP-6NO0972>
2020-09-21 17:53:16 +0530

    lead

---
+++ src/main/java/com/pk/controller/LeadDetails.java
@@ -0,0 +1,74 @@
+package com.pk.controller;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="lead_details")
+public class LeadDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long id;
+	
+	String name;
+	
+	String email;
+	
+	String mobileNo;
+	
+	String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/pk/controller/LeadDetailsRepository.java
@@ -0,0 +1,7 @@
+package com.pk.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LeadDetailsRepository extends JpaRepository<LeadDetails, Long>{
+
+}
