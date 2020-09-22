01059a897892648736d9fd7e492d8604eaf7f3c0
Praveen <Praveen@DESKTOP-6NO0972>
2020-09-22 17:05:33 +0530

    pradfsj

--- src/main/java/com/pk/controller/LeadDetails.java
+++ src/main/java/com/pk/controller/LeadDetails.java
@@ -6,11 +6,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 @Entity
 @Table(name="lead_details")
-@JsonIgnoreProperties
 public class LeadDetails {
 
 	@Id
