cca9bee02ecdc41c5cd04748bceee96125bf6800
Praveen <Praveen@DESKTOP-6NO0972>
2020-09-22 16:27:34 +0530

    sjdlajdlas

--- src/main/java/com/pk/controller/LeadDetails.java
+++ src/main/java/com/pk/controller/LeadDetails.java
@@ -6,69 +6,86 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name="lead_details")
+@JsonIgnoreProperties
 public class LeadDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	Long id;
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String fullName;
+	private String email;
+	private String mobileNo;
+	private String password;
+	private String subscribed;
+	private String whereFrom;
+	private String description;
 	
-	String name;
-	
-	String email;
-	
-	String mobileNo;
-	
-	String password;
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-	public String getName() {
-		return name;
+	public String getFirstName() {
+		return firstName;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
-
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getMobileNo() {
 		return mobileNo;
 	}
-
 	public void setMobileNo(String mobileNo) {
 		this.mobileNo = mobileNo;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public String getSubscribed() {
+		return subscribed;
+	}
+	public void setSubscribed(String subscribed) {
+		this.subscribed = subscribed;
+	}
+	public String getWhereFrom() {
+		return whereFrom;
+	}
+	public void setWhereFrom(String whereFrom) {
+		this.whereFrom = whereFrom;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	
 }
--- src/main/java/com/pk/controller/LeadsController.java
+++ src/main/java/com/pk/controller/LeadsController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,9 +16,9 @@ public class LeadsController {
 	@Autowired
 	LeadDetailsRepository repo;
 	
-	
+	@CrossOrigin(origins = "http://localhost:8080")
 	@RequestMapping(value = "/get", method = RequestMethod.GET)
-    public Object get(){
+    public List<LeadDetails> get(){
 		String status="";
 		List<LeadDetails> res = new ArrayList<>();
 		try {
@@ -27,8 +28,37 @@ public class LeadsController {
 		return res;
 	}
 	
-	@RequestMapping(value = "/post", method = RequestMethod.PUT)
+	@CrossOrigin(origins = "https://www.quantumnex.com")
+	@RequestMapping(value = "/live/get", method = RequestMethod.GET)
+    public List<LeadDetails> getLive(){
+		String status="";
+		List<LeadDetails> res = new ArrayList<>();
+		try {
+			 res = repo.findAll();
+		} catch (Exception e) {
+           e.printStackTrace();		}
+		return res;
+	}
+	
+	
+	@CrossOrigin(origins = "http://localhost:8080")
+	@RequestMapping(value = "/post", method = RequestMethod.POST)
     public String post(@RequestBody LeadDetails lead){
+		String status="";
+		try{
+		LeadDetails res = repo.save(lead);
+		status = "SUCCESS";
+		}catch(Exception e){
+			status = "FAILED";
+			e.printStackTrace();
+		}
+		return status;
+	}
+	
+	
+	@CrossOrigin(origins = "https://www.quantumnex.com")
+	@RequestMapping(value = "/live/post", method = RequestMethod.POST)
+    public String savelive(@RequestBody LeadDetails lead){
 		String status="";
 		try{
 		LeadDetails res = repo.save(lead);
