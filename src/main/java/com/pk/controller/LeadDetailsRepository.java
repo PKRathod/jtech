e85be809d6983b30253caeaf10f84eb296944fa6
Praveen <Praveen@DESKTOP-6NO0972>
2020-09-23 10:32:53 +0530

    sfsf

--- src/main/java/com/pk/controller/LeadDetailsRepository.java
+++ src/main/java/com/pk/controller/LeadDetailsRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface LeadDetailsRepository extends JpaRepository<LeadDetails, Long>{
 
-	@Query("FROM LeadDetails l WHERE l.registrationdDate >= DATEADD(day, -5, GetDate()) ORDER BY l.registrationdDate DESC")
+	@Query("FROM LeadDetails l WHERE l.registrationdDate >= DATEADD(l.registrationdDate, -5, GetDate()) ORDER BY l.registrationdDate DESC")
 	List<LeadDetails> getLatestDetail();
 	
 }
