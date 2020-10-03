45e0240a4ea7deeb3b328e7a7dc274dc3ac73211
Praveen <Praveen@DESKTOP-6NO0972>
2020-10-03 18:43:19 +0530

    sdsd

--- src/main/java/com/pk/controller/LeadsController.java
+++ src/main/java/com/pk/controller/LeadsController.java
@@ -18,75 +18,87 @@ public class LeadsController {
 
 	@Autowired
 	LeadDetailsRepository repo;
-	
+
 	@CrossOrigin(origins = "https://www.quantumnex.com")
 	@RequestMapping(value = "/wakeup", method = RequestMethod.GET)
-    public String wakeup(){
+	public String wakeup(){
 		System.out.println("::::::::::::::::::::::wakeup called:::::::::::::::::::::::::::::");
 		return "wakeup";
 	}
-	
+
 	@CrossOrigin(origins = "https://www.quantumnex.com")
 	@RequestMapping(value = "/live/get", method = RequestMethod.GET)
-    public BaseDto getLive(){
+	public BaseDto getLive(){
 		System.out.println(":::::::::::::::::::GetLive called::::::::::::::::::::::::::::::");
 		BaseDto base = new BaseDto();
 		List<LeadDetails> res = new ArrayList<>();
 		try {
-			 res = repo.getLatestDetail();
-			 base.setResponse(res);
+			res = repo.getLatestDetail();
+			base.setResponse(res);
 		} catch (Exception e) {
-           e.printStackTrace();		}
+			e.printStackTrace();		}
 		return base;
 	}
-	
-	
+
+
 	@CrossOrigin(origins = "http://localhost:8080")
 	@RequestMapping(value = "/post", method = RequestMethod.POST)
-    public BaseDto savelocal(@RequestBody LeadDetails lead){
+	public BaseDto savelocal(@RequestBody LeadDetails lead){
 		System.out.println("savelocal called..");
 		BaseDto base = new BaseDto();
 		try{
 			/*SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		       Date date = new Date(System.currentTimeMillis());
 		       lead.setRegistrationdDate(formatter.format(date));*/
-			
+
 			long ts = System.currentTimeMillis();
-		    Date localTime = new Date(ts);
-		    String format = "yyyy/MM/dd HH:mm:ss";
-		    SimpleDateFormat sdf = new SimpleDateFormat(format);   
-	       
-		    Date gmtTime = new Date(sdf.format(localTime));
-		 // Convert UTC to Local Time
-		    Date fromGmt = new Date(gmtTime.getTime() + TimeZone.getDefault().getOffset(localTime.getTime()));
-		  
-		    
+			Date localTime = new Date(ts);
+			String format = "yyyy/MM/dd HH:mm:ss";
+			SimpleDateFormat sdf = new SimpleDateFormat(format);   
+
+			Date gmtTime = new Date(sdf.format(localTime));
+			// Convert UTC to Local Time
+			Date fromGmt = new Date(gmtTime.getTime() + TimeZone.getDefault().getOffset(localTime.getTime()));
+
+
 			lead.setRegistrationdDate(fromGmt.toString());
-		repo.save(lead);
-		base.setResponse("SUCCESS");  
+			repo.save(lead);
+			base.setResponse("SUCCESS");  
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		return base;
 	}
-	
-	
+
+
 	@CrossOrigin(origins = "https://www.quantumnex.com")
 	@RequestMapping(value = "/live/post", method = RequestMethod.POST)
-    public BaseDto savelive(@RequestBody LeadDetails lead){
+	public BaseDto savelive(@RequestBody LeadDetails lead){
 		System.out.println("savelive called..");
 		BaseDto base = new BaseDto();
 		try{
-			SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			/*SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		       Date date = new Date(System.currentTimeMillis());
-			lead.setRegistrationdDate(formatter.format(date));
-		 repo.save(lead);
-		 base.setResponse("SUCCESS");
+			lead.setRegistrationdDate(formatter.format(date));*/
+
+			long ts = System.currentTimeMillis();
+			Date localTime = new Date(ts);
+			String format = "yyyy/MM/dd HH:mm:ss";
+			SimpleDateFormat sdf = new SimpleDateFormat(format);   
+
+			Date gmtTime = new Date(sdf.format(localTime));
+			// Convert UTC to Local Time
+			Date fromGmt = new Date(gmtTime.getTime() + TimeZone.getDefault().getOffset(localTime.getTime()));
+
+			lead.setRegistrationdDate(fromGmt.toString());
+
+			repo.save(lead);
+			base.setResponse("SUCCESS");
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		return base;
 	}
-	
+
 }
 
